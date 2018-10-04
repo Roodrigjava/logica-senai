0ee366f0a724fd3d26cb5030cb1a4d6d8ca4a29f
jlog180924 <jlog180924@info.local>
2018-10-03 21:59:28 -0300

    aula 008

--- aulas/workspace/loops/src/execicios/Ex005Triangulos.java
+++ aulas/workspace/loops/src/execicios/Ex005Triangulos.java
@@ -2,12 +2,6 @@ package execicios;
 
 public class Ex005Triangulos {
 	public static void main(String[] args) {
-		int i = 10;
 		
-		while (i > 0) {
-			System.out.println(i);
-			--i;
-		}
-
 	}
 }
